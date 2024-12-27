46c7552b931dcfd62bea5ed0dddf724d76d3f88e
Bal krishna Thapa Chhetri <118071778+BalThapa@users.noreply.github.com>
2024-12-27 20:08:42 +0200

    API calling WIP

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import dotenv from "dotenv";
 import express from "express";
 import axios from "axios";
 
@@ -6,12 +7,24 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-const API_URL = "https://api.openweathermap.org/data/2.5"
-
-const API_key =
+const API_URL = "https://api.openweathermap.org"
+const config = {
+    headers:{ Authorization : process.env.API_KEY},
+}
 
 app.get("/", (req, res)=> {
    res.render("index.ejs", { Report: 'Waiting for umbrella requirements...'})
+});
+
+app.post("/rain-status", async (req, res)=>{
+    const searchCity = req.body.city;
+    try {
+        const result = await axios.get (`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99`, config)
+        console.log(result);
+        res.render("index.ejs", {Report: JSON.stringify(result.data)})
+    } catch (error) {
+        res.render("index.ejs", {Report: JSON.stringify(error.response.data)})
+    }
 })
 
 
