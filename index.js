e71acf5ed7439406530aaf705ac833088f4b3a0d
Bal krishna Thapa Chhetri <118071778+BalThapa@users.noreply.github.com>
2024-12-29 18:43:40 +0200

    Zip code for the city is working but city name yet to work

--- index.js
+++ index.js
@@ -7,21 +7,31 @@ const port = 3000;
 
 app.use(express.static("public"));
 
+const APIKey = "a3a47352717128a7d35002a279546378"
+
 const API_URL = "https://api.openweathermap.org"
-const config = {
-    headers:{ Authorization : process.env.API_KEY},
-}
+// const config = {
+//     headers:{ Authorization : `appid`},
+// }
+
+
 
 app.get("/", (req, res)=> {
    res.render("index.ejs", { Report: 'Waiting for umbrella requirements...'})
 });
 
 app.post("/rain-status", async (req, res)=>{
-    const searchCity = req.body.city;
+    // const cityName = req.body.city;
     try {
-        const result = await axios.get (`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99`, config)
-        console.log(result);
-        res.render("index.ejs", {Report: JSON.stringify(result.data)})
+        const result = await axios.get (API_URL + "/data/2.5/weather/", {
+            params:{
+                zip: 33130, 
+                appid: APIKey,
+            }
+        })
+        res.render("index.ejs", {Report: JSON.stringify(result.data.weather) }
+        )
+       
     } catch (error) {
         res.render("index.ejs", {Report: JSON.stringify(error.response.data)})
     }
