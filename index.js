7bac47b64a99d7fe89c02f741a6d49dad42415cb
Bal krishna Thapa Chhetri <118071778+BalThapa@users.noreply.github.com>
2024-12-29 20:25:20 +0200

    The city name is working

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-import dotenv from "dotenv";
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}))
 
-const APIKey = "a3a47352717128a7d35002a279546378"
+ const APIKey = "a3a47352717128a7d35002a279546378"
+// const APIKey = {
+//     headers: {Authorization : process.env.API_KEY}
+// }
 
 const API_URL = "https://api.openweathermap.org"
 // const config = {
@@ -21,11 +25,11 @@ app.get("/", (req, res)=> {
 });
 
 app.post("/rain-status", async (req, res)=>{
-    // const cityName = req.body.city;
+     const cityName = req.body.city_name;
     try {
         const result = await axios.get (API_URL + "/data/2.5/weather/", {
             params:{
-                zip: 33130, 
+                q: cityName, 
                 appid: APIKey,
             }
         })
